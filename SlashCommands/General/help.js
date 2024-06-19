7079f31219b764676afb65a9a40b551054557079
Devil Dev <84636715+MAPXOR12@users.noreply.github.com>
2024-06-19 23:27:03 +0300

    Add files via upload

---
+++ SlashCommands/General/help.js
@@ -0,0 +1,31 @@
+const { MessageEmbed } = require("discord.js")
+let { splitMessage } = require("../../Functions/utils.js")
+
+module.exports = {
+  name:`help`,
+  description: 'Feeling lost?',
+  type: 'CHAT_INPUT',
+  botperms:["EMBED_LINKS"],
+  cooldown:5,
+  run:async(client, interaction,args) => {
+    let commands = client.slashCommands.map(command =>
+      `**● | /${command.name}** - **${command.description}**`).join("\n")
+
+    let embed = new MessageEmbed()
+    .setTitle(`Help Menu`) // By Ahmed Abd El-Latif Gaming
+    .setAuthor({name:`${interaction.user.tag}`,
+     iconURL:interaction.user.avatarURL()})
+    .setColor(`RED`)
+    .setThumbnail(client.user.avatarURL())
+    .setTimestamp()
+    .setFooter({text:`This Bot Made By Ahmed Abd El-Latif Gaming`});
+    let split = splitMessage(`${commands}\n`,{
+      char:"\n",
+      maxLength:1950
+    })
+    split.map(async(s,i) => {
+      embed.setDescription(s)
+      i === 0 ? interaction.reply({embeds:[embed]}).catch(err=>0) : interaction.channel.send({embeds:[embed]}).catch(err=>0)
+      })  
+  }
+}
